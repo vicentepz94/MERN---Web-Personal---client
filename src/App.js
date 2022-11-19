da469cf9634dad9c1cc3343d967eeb6016794dfe
vicentepz94 <vicentepz94@gmail.com>
2022-11-19 11:54:37 -0300

    79 Instalando semantic UI React (En el index.js se eliminan las etiquetas <react.strictmode> por 'deprecated'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
 
 export default function App() {
   return (
     <div>
       <h1>Estamos en el test</h1>
+      <div>
+        <Button content="Primary" primary />
+        <Button content="Secondary" secondary />
+      </div>
     </div>
   );
 }
