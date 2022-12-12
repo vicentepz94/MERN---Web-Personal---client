45a5e5843df88b33d6b4ee000361469447ff1fc9
vicentepz94 <vicentepz94@gmail.com>
2022-12-12 20:33:43 -0300

    122 Desactivando y activando usuario

--- src/components/Admin/Users/UserItem/UserItem.js
+++ src/components/Admin/Users/UserItem/UserItem.js
@@ -2,21 +2,54 @@ import React, { useState } from "react";
 import { Image, Button, Icon, Confirm } from "semantic-ui-react";
 import { image } from "../../../../assets";
 import { ENV } from "../../../../utils";
+import { useAuth } from "../../../../hooks";
 import { BasicModal } from "../../../Shared";
+import { User } from "../../../../api";
 import { UserForm } from "../UserForm";
 import "./UserItem.scss";
 
+const userController = new User();
+
 export function UserItem(props) {
   const { user, onReload } = props;
+  const { accessToken } = useAuth();
+
   const [showModal, setShowModal] = useState(false);
-  const [titleModal, setTitleModal] = useState(false);
+  const [titleModal, setTitleModal] = useState("");
+
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [confirmMessage, setConfirmMessage] = useState("");
+  const [isDelete, setIsDelete] = useState(false);
 
   const onOpenCloseModal = () => setShowModal((prevState) => !prevState);
+  const onOpenCloseConfirm = () => setShowConfirm((prevstate) => !prevstate);
 
   // Al hacer click en el lapiz de modificación se ejecutara la siguiente funcion
   const openUpdateUser = () => {
     setTitleModal(`Actualizar ${user.email}`);
     onOpenCloseModal();
+  };
+
+  const openDesactivateActivateConfirm = () => {
+    setIsDelete(false);
+    setConfirmMessage(
+      user.active
+        ? `Desactivar usuario ${user.email}`
+        : `Activar usuario ${user.email}`
+    );
+    onOpenCloseConfirm();
+  };
+
+  const onActivateDesactivate = async () => {
+    try {
+      await userController.updateUser(accessToken, user._id, {
+        active: !user.active,
+      });
+      onReload();
+      onOpenCloseConfirm();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -40,7 +73,11 @@ export function UserItem(props) {
           <Button icon primary onClick={openUpdateUser}>
             <Icon name="pencil" />
           </Button>
-          <Button icon color={user.active ? "orange" : "teal"}>
+          <Button
+            icon
+            color={user.active ? "orange" : "teal"}
+            onClick={openDesactivateActivateConfirm}
+          >
             <Icon name={user.active ? "ban" : "check"} />
           </Button>
           <Button icon color="red">
@@ -52,6 +89,16 @@ export function UserItem(props) {
       <BasicModal show={showModal} close={onOpenCloseModal} title={titleModal}>
         <UserForm close={onOpenCloseModal} onReload={onReload} user={user} />
       </BasicModal>
+
+      <Confirm
+        open={showConfirm}
+        onCancel={onOpenCloseConfirm}
+        onConfirm={
+          isDelete ? () => console.log("Confirm delete") : onActivateDesactivate
+        }
+        content={confirmMessage}
+        size="mini"
+      />
     </>
   );
 }
