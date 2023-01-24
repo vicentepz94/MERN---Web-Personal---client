aeb6abbeb98aeedce7a0b8848519afd1df94539e
vicentepz94 <vicentepz94@gmail.com>
2023-01-23 22:48:14 -0300

    134 Estructura de la pagina

--- src/pages/admin/Courses/Courses.js
+++ src/pages/admin/Courses/Courses.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Tab, Button } from "semantic-ui-react";
+import { BasicModal } from "../../../components/Shared";
+import "./Courses.scss";
 
 export function Courses() {
+  const [showModal, setShowModal] = useState(false);
+  const onOpenCloseModal = () => setShowModal((prevState) => !prevState);
   return (
-    <div>
-      <h1>Estamos en la pagina de Courses</h1>
-    </div>
+    <>
+      <div className="courses-page">
+        <div className="courses-page__add">
+          <Button primary onClick={onOpenCloseModal}>
+            Nuevo curso
+          </Button>
+        </div>
+        <Tab.Pane attached={false}>
+          <p>Lista de cursos</p>
+        </Tab.Pane>
+      </div>
+      <BasicModal show={showModal} close={onOpenCloseModal} title="Crear curs">
+        <p>Formulario para crear cursos</p>
+      </BasicModal>
+    </>
   );
 }
