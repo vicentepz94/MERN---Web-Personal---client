a2441dcfc7b261bf6b64e3cbbdc62bde039e575a
vicentepz94 <vicentepz94@gmail.com>
2023-02-03 17:49:41 -0300

    136 Añadiendo paginación

--- src/components/Admin/Course/ListCourses/ListCourses.js
+++ src/components/Admin/Course/ListCourses/ListCourses.js
@@ -1,34 +1,56 @@
 import React, { useState, useEffect } from "react";
 import { size, map } from "lodash";
-import { Loader } from "semantic-ui-react";
+import { Loader, Pagination } from "semantic-ui-react";
 import { Course } from "../../../../api";
 import { CourseItem } from "../CourseItem";
+import "./ListCourses.scss";
 
 const courseController = new Course();
 
 export function ListCourses() {
   const [courses, setCourses] = useState(false);
+  const [page, setPage] = useState(1);
+  const [pagination, setPagination] = useState();
 
   useEffect(() => {
     (async () => {
       try {
-        const response = await courseController.getCourses();
+        const response = await courseController.getCourses({ page, limit: 5 });
         setCourses(response.docs);
+        setPagination({
+          limit: response.limit,
+          page: response.page,
+          pages: response.pages,
+          total: response.total,
+        });
       } catch (error) {
         console.log(error);
       }
     })();
-  }, []);
+  }, [page]);
+
+  const changePage = (_, data) => {
+    setPage(data.activePage);
+  };
 
   if (!courses) return <Loader active inline="centered" />;
   if (size(courses) === 0) return "No hay ningún curso";
 
   return (
-    <div>
+    <div className="list-courses">
       {map(courses, (course) => (
         <CourseItem key={course._id} course={course} />
       ))}
-      <div>{/* TO DO: Incluir paginacion */}</div>
+      <div className="list-courses__pagination">
+        <Pagination
+          totalPages={pagination.pages}
+          defaultActivePage={pagination.page}
+          ellipsisItem={null}
+          firstItem={null}
+          lastItem={null}
+          onPageChange={changePage}
+        />
+      </div>
     </div>
   );
 }
