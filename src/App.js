2227e2dd66da0366bd6214745c171639ba7273df
Vicente Paredes <vicentepz94@gmail.com>
2023-04-11 00:55:56 -0400

    se agrega nueva build y configuracion homepage, ademas de cambios al webBrowser a hashbrowser as webbrowser

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 import { WebRouter, AdminRouter } from "./router";
 import { AuthProvider } from "./contexts";
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
-const SERVER_IP = "mern-web-personal-server-production.up.railway.app";
+const SERVER_IP = "webpersonalcurso.herokuapp.com";
 
 export const ENV = {
-  BASE_PATH: `https://${SERVER_IP}`,
-  BASE_API: `https://${SERVER_IP}/api/v1`,
+  BASE_PATH: `http://${SERVER_IP}`,
+  BASE_API: `http://${SERVER_IP}/api/v1`,
   API_ROUTES: {
     REGISTER: "auth/register",
     LOGIN: "auth/login",
